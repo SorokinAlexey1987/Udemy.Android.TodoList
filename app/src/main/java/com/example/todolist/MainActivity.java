8acb2c06189c31cb44c9b0c91f327ab1d57b17e7
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-03-20 22:49:59 +0300

    FirstScreen

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -31,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
             Note note = new Note(i, "Note " + i, random.nextInt(3));
             notes.add(note);
         }
+
+        showNotes();
     }
 
     private void initViews() {
@@ -40,7 +44,27 @@ public class MainActivity extends AppCompatActivity {
 
     private void showNotes() {
         for (Note note : notes) {
-            View view = getLayoutInflater().inflate(R.layout.note_item, linearLayoutNotes, false);
+            View view = getLayoutInflater().inflate(
+                    R.layout.note_item,
+                    linearLayoutNotes,
+                    false
+            );
+            TextView textViewNote = view.findViewById(R.id.textViewNote);
+            textViewNote.setText(note.getText());
+
+            int colorResId;
+            switch (note.getPriority()) {
+                case 0:
+                    colorResId = android.R.color.holo_green_light;
+                    break;
+                case 1:
+                    colorResId = android.R.color.holo_orange_light;
+                    break;
+                default:
+                    colorResId = android.R.color.holo_red_light;
+            }
+            int color = ContextCompat.getColor(this, colorResId);
+            textViewNote.setBackgroundColor(color);
             linearLayoutNotes.addView(view);
         }
     }
